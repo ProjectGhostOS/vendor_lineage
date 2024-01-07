ffd9bd39395e510820a945cb857f17039b41766d
Ghosuto <clash.raja10@gmail.com>
2024-01-07 07:17:27 +0100

    vendor: Bumpup version

--- config/version.mk
+++ config/version.mk
@@ -1,8 +1,8 @@
 PRODUCT_VERSION_MAJOR = 14
-PRODUCT_VERSION_MINOR = 1
+PRODUCT_VERSION_MINOR = 3
 
 # Increase CR Version with each major release.
-CR_VERSION := Muryōkūsho
+CR_VERSION := Jorogumo
 
 # Internal version
 LINEAGE_VERSION := GhostOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(CR_VERSION)-$(GHOST_EDITION)
